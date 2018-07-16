bd36f94e943d5ea6fe0a17fafb8025e3ebba9104
Charlotte Morrison <charlottemaemorrison@gmail.com>
2018-07-16 11:35:33 -0400

    Added sample file

---
+++ Main.java
@@ -0,0 +1,24 @@
+/*Use both sysem.out statement in your project:
+	System.out.println("");
+	System.out.print("");
+*/
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//print your name and todays date
+		
+		//print your address
+		
+		//print your phone number
+		
+		//print your schedule of courses
+		
+		//share some information about yourself (hobbies, sports, teams, clubs, etc.)
+		
+		//print an explanation of the difference between System.out.println and System.out.print
+
+	}
+
+}
